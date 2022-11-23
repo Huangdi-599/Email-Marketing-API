05401bb32b45c511d2627ebaaaf55145548332fd
Huangdi-599 <92282845+Huangdi-599@users.noreply.github.com>
2022-11-23 14:14:20 +0100

    created Models

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Lists, EmailContact
+
+
 
 # Register your models here.
+admin.site.register(Lists)
+admin.site.register(EmailContact)
--- api/models.py
+++ api/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.conf import settings
+
+User = settings.AUTH_USER_MODEL ##Authenticated User
 
 # Create your models here.
+class Lists (models.Model):
+    user = models.ForeignKey(User,  default = 1 ,null = True,  on_delete=models.SET_NULL)
+    name = models.CharField(max_length=254)
+
+class EmailContact(models.Model):
+    user = models.ForeignKey(User,  default = 1 ,null = True,  on_delete=models.SET_NULL)
+    email= models.EmailField(max_length=254)
+    lists = models.ManyToManyField(Lists)
+    reg_date = models.DateTimeField(auto_now_add=True)
+
+
